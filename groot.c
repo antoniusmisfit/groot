94a8923801151a9c7ca91a26ae94087a35eac117
Tony Agudo <antoniusmisfit@gmail.com>
2023-08-07 14:37:21 -0400

    Create groot.c

---
+++ groot.c
@@ -0,0 +1,10 @@
+//Modified minimal Felker init to enable running in proo
+#include <signal.h>
+#include <unistd.h>
+
+int proot_mode(int argc, char* argv[]);
+
+int main(int argc,char* argv[])
+{
+        return execve("/etc/rc", (char *[]){ "rc", 0 }, (char *[]){ 0 });
+}
