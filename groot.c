d67e777139e44e5b5aa4ea60b85ff019974f180a
Tony Agudo <antoniusmisfit@gmail.com>
2023-08-08 00:49:29 -0400

    Implement minimal hybrid init
    Groot can now run as a normal init or within proot depending on whether it is PID-1 or not.

--- groot.c
+++ groot.c
@@ -6,5 +6,12 @@ int proot_mode(int argc, char* argv[]);
 
 int main(int argc,char* argv[])
 {
+        if(getpid()!=1) return proot_mode(argc,argv);
         return execve("/etc/rc", (char *[]){ "rc", 0 }, (char *[]){ 0 });
+}
+
+int proot_mode(int argc, char* argv[])
+{
+//proot-specific setup here
+return execve("/etc/rc", (char *[]){ "rc", 0 }, (char *[]){ 0 });
 }
